0b22d4ff100d97a9d503cbcf1a0fd42f7144b050
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-06 22:52:08 +0500

    q4 solved

--- 02/q4/main.ts
+++ 02/q4/main.ts
@@ -1,1 +1,1 @@
-console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”');
+console.log("Computers donot usually have a sense of if you have a picture of something what is in that image. And if we can do a good job of understanding what is in an image, that can bring along a lot of new things you can do in applications");
