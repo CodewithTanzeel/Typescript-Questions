a840dbd1f16bf3a27e0f20887a396673aadd54d6
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-06 23:08:57 +0500

    Update main.js

--- 04/q10/main.js
+++ 04/q10/main.js
@@ -1,6 +1,6 @@
-// QUESTION #9:
+// QUESTION #10:
 //--------------------------------------Program 1----------------------------------------
-var person = " \t \n  \t Marjan  \t \n \t"; //whitespaces
+var person = " \t \n  \t Tanzeel Ahmad \t \n \t"; //whitespaces
 // \t => tabular or horizontal space
 // \n => newline
 console.log(person);
@@ -10,3 +10,4 @@ console.log(6 + 2); //Addition
 console.log(12 - 4); //Subtraction
 console.log(4 * 2); //Multiplication
 console.log(72 / 9); //Division
+// both q9 and q8 at the same instance
