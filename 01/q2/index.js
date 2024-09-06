f2bf80793015d80d9fac3d5d82281220c57c31a5
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-06 22:43:57 +0500

    Solved q1

--- 01/q2/index.js
+++ 01/q2/index.js
@@ -1,5 +1,5 @@
 // QUESTION #1:
 // Personal Message: Store a person’s name in a variable and print a message to them, like
-// “Hello Marjan, would you like to learn some TypeScript today?”
-var person = "Marjan Ahmed"; //store the value (name)
+// “Hello"TanzeelAhmad you like to learn some TypeScript today?”
+var person = "TanzeelAhmad"; //store the value (name)
 console.log("Hello ".concat(person, ", would you like to learn some TypeScript today?\u201D")); //print it by y template litteral
--- 01/q2/index.ts
+++ 01/q2/index.ts
@@ -1,6 +1,6 @@
 // QUESTION #1:
 // Personal Message: Store a person’s name in a variable and print a message to them, like
-// “Hello Marjan, would you like to learn some TypeScript today?”
+// “Hello"TanzeelAhmad you like to learn some TypeScript today?”
 
-let person: string = "Marjan Ahmed"; //store the value (name)
+let person: string = "TanzeelAhmad"; //store the value (name)
 console.log(`Hello ${person}, would you like to learn some TypeScript today?”`); //print it by y template litteral
