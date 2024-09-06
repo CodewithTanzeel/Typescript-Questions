074a1754c338bc17a1e25695e2f59a0a5d031530
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-06 22:53:00 +0500

    Update index.ts

--- 02/q5/index.ts
+++ 02/q5/index.ts
@@ -1,5 +1,5 @@
 // QUESTION #5:
-let famous_person = "Elon Musk"
+let famous_person = "Jeff Dean";
 let message = "Take Risk Now. Do something bold. You won't regret it.";
 
-console.log(`'${famous_person} once said, "${message}"'`)
+console.log(`'${famous_person} once said, "${message}"'`);
