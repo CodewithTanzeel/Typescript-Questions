c1431acac2f73d69fd8670961098b81dfe460b5b
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-07 17:07:39 +0500

    Update index.ts

--- 05/q15/index.ts
+++ 05/q15/index.ts
@@ -1,9 +1,9 @@
 //QUESTION #15:
 
 // Guest List
-let guests : string[] = ['Yahya', "Haris", 'Shaheen', "Shahid"];
+let guests: string[] = ["Atiqa", "Arham", "Ovais", "Imtisaal"];
 
-let unableToAttend = "Shaheen";
+let unableToAttend = "Ovais";
 console.log(`${unableToAttend} can't make it to dinner.`);
 
 // Replace the guest
@@ -11,6 +11,6 @@ let newGuest = "Fatimah";
 guests[guests.indexOf(unableToAttend)] = newGuest;
 
 // New invitations
-guests.forEach(guest => {
-    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+guests.forEach((guest) => {
+  console.log(`Dear ${guest}, would you like to join me for dinner?`);
 });
