4c47000f34e0319abaddfbd383ae2108245a7a86
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-07 19:17:45 +0500

    Update main.ts

--- 06/q18/main.ts
+++ 06/q18/main.ts
@@ -1,2 +1,8 @@
-let guests: string[] = ["Albert Einstein", "Marie Curie", "Leonardo da Vinci", "Isaac Newton", "Charles Darwin"];
-console.log(`I am inviting ${guests.length} people to dinner.`);
+let guests: string[] = [
+  "Albert Einstein",
+  "Marie Curie",
+  "Leonardo da Vinci",
+  "Isaac Newton",
+  "Charles Darwin",
+];
+console.log(`I have invited ${guests.length} people to dinner.`);
