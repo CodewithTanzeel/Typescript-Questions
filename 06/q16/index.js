3d9019b1f7eeaf8028de82c337dcbdfc2c7da675
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-07 19:08:12 +0500

    .

--- 06/q16/index.js
+++ 06/q16/index.js
@@ -1,11 +1,12 @@
-var guests = ['Yahya', 'Haris', 'Fatimah', 'Shahid'];
+var guests = ["Ali", "Ashir", "zehra"];
+//0      1         2
 // Unfortunately I found a big table for inviting more guests
 // Invite at the beginning
-var add1 = guests.unshift('Meraaj');
+var add1 = guests.unshift("Zehra");
 // Inviting at the middle
-var add2 = guests.splice(guests.length / 2, 0, "Zubaida");
+var add2 = guests.splice(guests.length / 2, 0, "Zubi");
 // Inviting at the end
-var add3 = guests.push("Usman");
+var add3 = guests.push("Saad");
 guests.forEach(function (guest) {
     console.log("".concat(guest, ", would you like to come to my dinner party?"));
 });
--- 06/q16/index.ts
+++ 06/q16/index.ts
@@ -1,16 +1,16 @@
-let guests: string[] = ['Yahya', 'Haris', 'Fatimah', 'Shahid'];
-
+let guests: string[] = ["Ali", "Ashir", "zehra"];
+                        //0      1         2
 // Unfortunately I found a big table for inviting more guests
 
 // Invite at the beginning
-let add1 = guests.unshift('Meraaj')
+let add1 = guests.unshift("Zehra");
 
 // Inviting at the middle
-let add2 = guests.splice(guests.length/2,0,"Zubaida")
+let add2 = guests.splice(guests.length / 2, 0, "Zubi");
 
 // Inviting at the end
-let add3 = guests.push("Usman")
+let add3 = guests.push("Saad");
 
-guests.forEach(guest => {
-    console.log(`${guest}, would you like to come to my dinner party?`);
-})
+guests.forEach((guest) => {
+  console.log(`${guest}, would you like to come to my dinner party?`);
+});
