9bb24a5f30b00749233db00111105d2d15f1172e
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-07 16:42:37 +0500

    .

--- 04/q12/main.ts
+++ 04/q12/main.ts
@@ -1,6 +1,6 @@
 // QUESTION #12
-let names = ['Ahmed', 'Nusrat', 'Raza', 'Jamil', 'kate'];
+let names = ["Ahmed", "Nusrat", "Raza", "Jamil", "kate"];
 
-for(let i = 0; i < names.length; i++){
-    console.log(`Hi ${names[i]}, would you like to drink some coffee?`)
-};
+for (let i = 0; i < names.length; i++) {
+  console.log(`Hi ${names[i]}, would you like to drink some coffee?`);
+}
