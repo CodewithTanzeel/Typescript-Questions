75bc3bb3badca95e126d3d88a462398ea60c3f51
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-07 16:41:55 +0500

    q12 solved

--- 04/q12/main.ts
+++ 04/q12/main.ts
@@ -1,5 +1,5 @@
 // QUESTION #12
-let names = ['Ahmed', 'Sufyan', 'Raza', 'Anus', 'Yousuf'];
+let names = ['Ahmed', 'Nusrat', 'Raza', 'Jamil', 'kate'];
 
 for(let i = 0; i < names.length; i++){
     console.log(`Hi ${names[i]}, would you like to drink some coffee?`)
