26e58e4992568519a8dd75ff19a921be46d9b331
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-09 22:33:49 +0500

    Update index.ts

--- 07/q21/index.ts
+++ 07/q21/index.ts
@@ -1,8 +1,10 @@
 const car = {
-    name: 'BMW',
-    model: 'X5',
-    year: 2020,
-    color: 'black'
+  name: "Mercedes-Benz",
+  model: "CLS65",
+  year: 2017,
+  color: "black",
 };
 
-console.log(`The car is: ${car.name}, it's model is: ${car.model}, it's year is ${car.year}, and it's color is ${car.color}`);
+console.log(
+  `The car is: ${car.name}, it's model is: ${car.model}, it's year is ${car.year}, and it's color is ${car.color}`
+);
