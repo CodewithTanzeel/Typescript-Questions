ccfa4044931468231a46fd410242501fe3425486
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-09 22:26:07 +0500

    Update main.js

--- 06/q18/main.js
+++ 06/q18/main.js
@@ -1,2 +1,10 @@
-var guests = ["Albert Einstein", "Marie Curie", "Leonardo da Vinci", "Isaac Newton", "Charles Darwin"];
-console.log("I am inviting ".concat(guests.length, " people to dinner."));
+var guests = [
+    "Albert Einstein",
+    "Marie Curie",
+    "Leonardo da Vinci",
+    "Isaac Newton",
+    "Charles Darwin",
+    "Max Plank",
+    "Heisen Berg",
+];
+console.log("I have invited ".concat(guests.length, " people to dinner."));
