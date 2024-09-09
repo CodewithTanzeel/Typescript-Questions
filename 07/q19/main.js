592030661833ade9a1b6683a2ec7c8bc72f64117
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-09 22:30:23 +0500

    Update main.js

--- 07/q19/main.js
+++ 07/q19/main.js
@@ -1,2 +1,7 @@
-var guests = ["Albert Einstein", "Marie Curie", "Leonardo da Vinci", "Isaac Newton", "Charles Darwin"];
+var guests = [
+    "Albert Einstein",
+    "Marie Curie",
+    "Leonardo da Vinci",
+    "Isaac Newton",
+];
 console.log("I am inviting ".concat(guests.length, " people to dinner."));
