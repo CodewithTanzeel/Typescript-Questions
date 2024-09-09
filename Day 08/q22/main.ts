8f69abaf4f9c9001d04f6087fe20b1a6cb66d876
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-09 22:40:41 +0500

    Update main.ts

--- Day 08/q22/main.ts
+++ Day 08/q22/main.ts
@@ -1,5 +1,5 @@
-let friends: string[] = ["Salman", "Da'ood", "Yousuf"];
+let friends: string[] = ["Taha", "Da'ood", "Imtisaal"];
 
 console.log(friends[3]); // Intentional error (Out Of Bound Error): Arrays are zero-indexed, so index 3 is out of bounds.
 friends[3] = "Haziq"; // Correcting the error by accessing a valid index.
-console.log(friends)
+console.log(friends);
