5cdf304dced2a168c7474ea81d7da80919ba1cea
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-10 23:15:43 +0500

    .

--- 10/q29/app.ts
+++ 10/q29/app.ts
@@ -1,18 +1,16 @@
-let favoriteFruits: string[] = ['apple', 'mango', 'orange', 'peach'];
+let favoriteFruits: string[] = ["apple", "mango", "orange", "peach"];
 
-if(favoriteFruits.includes("apple")) {
-    console.log("I think you like apple.")
+if (favoriteFruits.includes("apple")) {
+  console.log("I think you like apple.");
 }
-if(favoriteFruits.includes("orange")) {
-    console.log("I think you like orange.")
+if (favoriteFruits.includes("orange")) {
+  console.log("I think you like orange.");
 }
-if(favoriteFruits.includes("mango")) {
-    console.log("I think you like mango.")
+if (favoriteFruits.includes("mango")) {
+  console.log("I think you like mango.");
 }
-if(favoriteFruits.includes("watermelon")) {
-    console.log("I think you like watermelon.")
-}
-
-else{
-    console.log("I don't think you like any of these fruits.")
+if (favoriteFruits.includes("peach")) {
+  console.log("I think you like peach.");
+} else {
+  console.log("I don't think you like any of these fruits.");
 }
