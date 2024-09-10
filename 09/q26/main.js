8106d7adf08787d711e5412eb9af7bc38ed1bf37
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-10 22:58:34 +0500

    .

--- 09/q26/main.js
+++ 09/q26/main.js
@@ -1,6 +1,5 @@
-"use strict";
 //Output Occur
-let alien_color = "green";
+var alien_color = "green";
 if (alien_color == "green") {
     console.log("You just earned 5 points for shooting the alien!");
 }
