51df2956e09a535cf0f0543603fc29e4c6e7d783
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-12 23:06:31 +0500

    .

--- 10/q30/main.ts
+++ 10/q30/main.ts
@@ -1,9 +1,9 @@
 let usernames: string[] = ["admin", "user1", "user2", "user3", "user4"];
 
-usernames.forEach(username => {
-    if (username.includes("admin")) {
-        console.log("Hello admin, would you like to see a some report?");
-    } else {
-        console.log(`Hello ${username}, thanks for visiting .`);
-    }
+usernames.forEach((username) => {
+  if (username.includes("admin")) {
+    console.log("Hello admin, would you like to see a some report?");
+  } else {
+    console.log(`Hello ${username}, thanks for visiting .`);
+  }
 });
