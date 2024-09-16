091c824b5f74b6cc3570a81c884f7c6696d007a8
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-16 23:37:47 +0500

    .

--- 18/q52/main.ts
+++ 18/q52/main.ts
@@ -1,12 +1,12 @@
 const smartphone = {
-    brand: "Apple",
-    model: "iPhone X",
-    screenSize: "6.1-inch",
-    camera: {
-        primary: "12 MP",
-        secondary: "8 MP",
-    }
-}
+  brand: "Apple",
+  model: "iPhone Xi",
+  screenSize: "6.1-inch",
+  camera: {
+    primary: "12 MP",
+    secondary: "12 MP",
+  },
+};
 
 // console.log(smartphone.brand, smartphone.model, smartphone.camera);
-console.log(smartphone)
+console.log(smartphone);
