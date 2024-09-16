c635d2b210da5c98a0abc32399a1be088edecae6
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-16 23:39:16 +0500

    .

--- 18/q53/main.ts
+++ 18/q53/main.ts
@@ -1,14 +1,13 @@
 const skills = {
-    languages: ['C', 'C++', 'TypeScript'],
-    frameworks: ['ReactJS', 'Angular.js', 'Vue.js'],
-    databases: ['MySQL', 'PostgreSQL', 'MongoDB'],
-    tools: ['Git', 'Version Control', 'Terminal'],
+  languages: ["C", "C#", "TypeScript"],
+  frameworks: ["ReactJS", "Angular.js", "Vue.js"],
+  databases: ["MySQL", "PostgreSQL", "MongoDB"],
+  tools: ["Git", "Version Control", "Terminal"],
 };
 
-const {languages,frameworks,databases,tools} = skills;
-
+const { languages, frameworks, databases, tools } = skills;
 
 console.log(`\nLanguages: ${languages[2]}, \n
 Frameworks: ${frameworks[0]}, \n
 Databases: ${databases[2]}, \n
-Tools: ${tools[0]}`)
+Tools: ${tools[0]}`);
