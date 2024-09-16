6132f2819696818af3aa496a2e237f1bb68266a6
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-16 23:43:36 +0500

    .

--- 19/q56/main.ts
+++ 19/q56/main.ts
@@ -1,6 +1,6 @@
-let mixItems = [1, "mango", 3, "orange", 5, "apple"];
+let mixItems = [1, "mango", 3, "orange", 6, "apple"];
 
-let onlyString = mixItems.filter((item) => typeof item === "string") 
+let onlyString = mixItems.filter((item) => typeof item === "string");
 
-console.log(mixItems) // Mix items
-console.log(onlyString) // Only string
+console.log(mixItems); // Mix items
+console.log(onlyString); // Only string
