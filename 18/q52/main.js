cff15d0392fab5945fce56f32d197566d0877a73
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-16 23:38:32 +0500

    .

--- 18/q52/main.js
+++ 18/q52/main.js
@@ -1,11 +1,11 @@
 var smartphone = {
     brand: "Apple",
-    model: "iPhone X",
+    model: "iPhone Xi",
     screenSize: "6.1-inch",
     camera: {
         primary: "12 MP",
-        secondary: "8 MP",
-    }
+        secondary: "12 MP",
+    },
 };
 // console.log(smartphone.brand, smartphone.model, smartphone.camera);
 console.log(smartphone);
