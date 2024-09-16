e1068a6d6905702918768ea49e41a2b8fd3f2609
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-16 23:44:22 +0500

    .

--- 19/q56/main.js
+++ 19/q56/main.js
@@ -1,4 +1,4 @@
-var mixItems = [1, "mango", 3, "orange", 5, "apple"];
+var mixItems = [1, "mango", 3, "orange", 6, "apple"];
 var onlyString = mixItems.filter(function (item) { return typeof item === "string"; });
 console.log(mixItems); // Mix items
 console.log(onlyString); // Only string
