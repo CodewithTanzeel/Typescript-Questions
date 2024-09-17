c8c42518bb74eb802c7e85b3f72d8fb5d8177652
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-17 22:54:57 +0500

    .

--- 19/q55/main.ts
+++ 19/q55/main.ts
@@ -1,6 +1,6 @@
 let numbers: number[] = [1, 2, 3, 4, 5, 6];
 
 // Doubles each number
-let doubledNumbers = numbers.map((number) => number * 2);
+let doubledNumbers = numbers.map((number) => number * 5);
 
-console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
+console.log(doubledNumbers); // Output: write your output here...!!
