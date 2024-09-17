acb516934d06d8e5f4fa38c04144383edbca1434
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-17 22:56:30 +0500

    .

--- 21/q61/main.ts
+++ 21/q61/main.ts
@@ -1,11 +1,11 @@
 enum Vehicle {
-    car,
-    truck,
-    bicycle, 
-    motorcycle
+  car,
+  truck,
+  bicycle,
+  airplane,
 }
 
 // console.log(Vehicle.bicycle);
-        // OR
-console.log(Vehicle['bicycle'])
-console.log(Vehicle[2])
+// OR
+console.log(Vehicle["bicycle"]);
+console.log(Vehicle[2]);
