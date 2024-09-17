c7d76845d50ac0e101ecf1e941e0cbc0e54f80ec
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-17 23:01:39 +0500

    .

--- 21/q62/main.js
+++ 21/q62/main.js
@@ -1,6 +1,6 @@
 var studentData = {
-    name: "Tanzeel Ahmad",
+    name: "TanzeelAhmad",
     age: 15,
-    courses: ["DevOps", " Soft-Development", "Cyber Security"],
+    courses: ["DevOps", "Soft-Development", "Cyber Security"],
 };
 console.log(studentData);
--- 21/q62/main.ts
+++ 21/q62/main.ts
@@ -1,10 +1,8 @@
 let studentData: {
   name: string;
-  age: number;
   courses: string[];
 } = {
   name: "TanzeelAhmad",
-  age: 15,
   courses: ["DevOps", "Soft-Development", "Cyber Security"],
 };
 
