99a4bfa57206c08d1a46e160173fe5cda207fe1b
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-17 23:10:10 +0500

    .

--- 25/q73/main.ts
+++ 25/q73/main.ts
@@ -1,8 +1,8 @@
 function variable() {
-    let value = 10; // Initially assigns the value 10
-    console.log("Initial value:", value); // Logs the initial value
-    value = 20; // Updates the value to 20
-    console.log("Updated value:", value); // Logs the updated value
+  let value = 12; // Initially assigns the value 10
+  console.log("Initial value:", value); // Logs the initial value
+  value = 50; // Updates the value to 20
+  console.log("Updated value:", value); // Logs the updated value
 }
 
-variable()
+variable();
