07111a8d907ea90b9f6acc2b1dafe799bd7d7b5f
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-17 22:51:42 +0500

    .

--- 19/q57/main.js
+++ 19/q57/main.js
@@ -1,4 +1,4 @@
-var studentsGrade = [80, 48, 43, 90, 76];
+var studentsGrade = [80, 48, 43, 80, 76];
 var averageGrade = studentsGrade.reduce(function (acc, grades) {
     return acc + grades / studentsGrade.length;
 }, 0);
