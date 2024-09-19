ce6a64a0245aade19c91a875832014d4efc2e0bf
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-19 21:50:06 +0500

    .

--- 72/TS-Generics/4-Generics-And-Interfaces.ts
+++ 72/TS-Generics/4-Generics-And-Interfaces.ts
@@ -1,43 +1,44 @@
-/***************************
- * GENERICS AND INTERFACES *
+/***************************  
+ * GENERICS AND INTERFACES *  
  ***************************/
 
-// Interface definitions for Book and Game
+// Interface definition for `Book`
 interface Book {
-    itemType: string;
-    title: string;
-    isbn: number;
+  itemType: string;  // Defines the type of item, e.g., "Book"
+  title: string;     // Title of the book
+  isbn: number;      // Unique ISBN number for the book
+}
+
+// Interface definition for `Game`
+interface Game {
+  itemType: string;  // Defines the type of item, e.g., "Game"
+  title: string;     // Title of the game
+  style: string;     // Genre or style of the game (e.g., "Action")
+  price: number;     // Price of the game
+}
+
+// Generic Class `Collection`, which works with a type `T`
+class Collection<T> {
+  public data: T[] = [];  // Array to hold a collection of items of type `T`
+
+  // Method to add an item of type `T` to the collection
+  add(item: T): void {
+    this.data.push(item);  // Adds the item to the `data` array
   }
-  
-  interface Game {
-    itemType: string;
-    title: string;
-    style: string;
-    price: number;
-  }
-  
-  // Generic Class `Collection`
-  class Collection<T> {
-    public data: T[] = [];
-  
-    // Method to add an item of type `T` to the collection
-    add(item: T): void {
-      this.data.push(item);
-    }
-  }
-  
-  // Creating an instance of `Collection` with type parameter `Book`
-  let itemOne = new Collection<Book>();
-  itemOne.add({ itemType: "Book", title: "Atomic Habits", isbn: 150510 });
-  itemOne.add({ itemType: "Book", title: "Follow Your Heart", isbn: 650650 });
-  console.log(itemOne);
-  
-  // Creating an instance of `Collection` with type parameter `Game`
-  let itemTwo = new Collection<Game>();
-  itemTwo.add({
-    itemType: "Game",
-    title: "Uncharted",
-    style: "Action",
-    price: 150,
-  });
-  console.log(itemTwo);
+}
+
+// Creating an instance of `Collection` with the type parameter `Book`
+let itemOne = new Collection<Book>();
+itemOne.add({ itemType: "Book", title: "Atomic Habits", isbn: 150510 });  // Adds a `Book` object
+itemOne.add({ itemType: "Book", title: "Follow Your Heart", isbn: 650650 });  // Adds another `Book` object
+console.log(itemOne);  // Logs the `Collection` of `Book` items
+
+// Creating an instance of `Collection` with the type parameter `Game`
+let itemTwo = new Collection<Game>();
+itemTwo.add({
+  itemType: "Game",  // Adds a `Game` object
+  title: "Uncharted",
+  style: "Action",
+  price: 150,
+});
+console.log(itemTwo);  // Logs the `Collection` of `Game` items
