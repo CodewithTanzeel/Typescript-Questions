be9457702b1ea19f731ac9106e69f1c52964c7bc
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-19 21:42:31 +0500

    .

--- 73/TS-Type-Assertions/1-Type-Assertions.ts
+++ 73/TS-Type-Assertions/1-Type-Assertions.ts
@@ -1,8 +1,8 @@
-/*******************
- * TYPE ASSERTIONS *
- *******************/
+/*******************  
+* TYPE ASSERTIONS *  
+*******************/  
 
-let data: any = "1000";
+let data: any = "1000";  // 'data' is of type 'any' and holds a string value "1000"
 
-// Type assertion to treat "data" as a string
-console.log((data as string).repeat(3));
+// Type assertion to treat "data" as a string and use string methods
+console.log((data as string).repeat(3));  // The string "1000" is repeated 3 times, output: "100010001000"
