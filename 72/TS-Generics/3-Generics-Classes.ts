43b60627ddb21e5ef4ef78392fd1a72293864799
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-19 21:48:26 +0500

    .

--- 72/TS-Generics/3-Generics-Classes.ts
+++ 72/TS-Generics/3-Generics-Classes.ts
@@ -2,22 +2,32 @@
  * GENERICS CLASSES *
  ********************/
 
-// Generic Class `User`
+// Generic Class `User` with a default type parameter of `string`
 class User<T = string> {
-    constructor(public value: T) {}
-  
-    // Method that takes a message of type `T` and displays it along with the `value` property
-    show(msg: T): void {
-      console.log(`${msg} - ${this.value}`);
-    }
+  constructor(public value: T) {
+    // The class constructor initializes the `value` property of type T
   }
-  
-  // Creating an instance of `User` with a specific type parameter (string)
-  let userOne = new User<string>("Elzero");
-  console.log(userOne.value); // Outputs: "Elzero"
-  userOne.show("Message"); // Outputs: "Message - Elzero"
-  
-  // Creating an instance of `User` with a type parameter that can be a number or a string
-  let userTwo = new User<number | string>(100);
-  console.log(userTwo.value); // Outputs: 100
-  userTwo.show("Message"); // Outputs: "Message - 100"
+
+  // Method `show` takes an argument `msg` of type T and logs it along with the `value`
+  show(msg: T): void {
+    console.log(`${msg} - ${this.value}`);
+  }
+}
+
+// Creating an instance of `User` with a type parameter `string`
+let userOne = new User<string>("Elzero");
+// The value property is of type string
+console.log(userOne.value);
+// Logs: "Elzero"
+
+userOne.show("Message");
+// Logs: "Message - Elzero", since both `msg` and `value` are strings
+
+// Creating an instance of `User` with a type parameter that allows `number` or `string`
+let userTwo = new User<number | string>(100);
+// The value property is of type `number | string`, currently assigned a number
+console.log(userTwo.value);
+// Logs: 100
+
+userTwo.show("Message");
+// Logs: "Message - 100", where `msg` is a string and `value` is a number
