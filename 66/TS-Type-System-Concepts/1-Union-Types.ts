da5fdf55e829e49e0344780ee50ecb9a21c5d5b4
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-20 23:29:30 +0500

    .

--- 66/TS-Type-System-Concepts/1-Union-Types.ts
+++ 66/TS-Type-System-Concepts/1-Union-Types.ts
@@ -2,13 +2,30 @@
  * UNION TYPES *
  ***************/
 
-let myVar: string | number;
+/**
+ * The variable `myVar` is declared with a union type that allows it to hold 
+ * either a `string` or a `number`.
+ */
+let myVar: string | number;  // Union type declaration
 
+/**
+ * Assigning a string value to `myVar`. This is a valid assignment
+ * because `myVar` is of type `string | number`.
+ */
 myVar = "Hello"; // Valid assignment
-console.log(myVar);
+console.log(myVar);  // Output: Hello
 
+/**
+ * Assigning a number value to `myVar`. This is also a valid assignment
+ * as it conforms to the declared union type.
+ */
 myVar = 42; // Valid assignment
-console.log(myVar);
+console.log(myVar);  // Output: 42
 
-// Uncommenting the line below would result in a TypeScript error
+/**
+ * The following line is commented out because it would result in a TypeScript error.
+ * 
+ * Attempting to assign a boolean value to `myVar` would cause a compile-time error
+ * since `boolean` is not included in the defined union type `string | number`.
+ */
 // myVar = true;  // Error: Type 'boolean' is not assignable to type 'string | number'.
