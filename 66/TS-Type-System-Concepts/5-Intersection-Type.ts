b8750fee1716e186f685e5de73cb456fb9f9354d
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-20 23:32:36 +0500

    .

--- 66/TS-Type-System-Concepts/5-Intersection-Type.ts
+++ 66/TS-Type-System-Concepts/5-Intersection-Type.ts
@@ -2,21 +2,40 @@
  * INTERSECTION TYPE *
  *********************/
 
+/**
+ * The `FirstType` type is defined as an object with two properties:
+ * - `name`: A string representing the person's name.
+ * - `age`: A number representing the person's age.
+ */
 type FirstType = {
-    name: string;
-    age: number;
-  };
-  
-  type SecondType = {
-    address: string;
-    phone: string;
-  };
-  
-  type CombinedType = FirstType & SecondType;
-  
-  const person: CombinedType = {
-    name: "John Doe",
-    age: 30,
-    address: "123 Main St",
-    phone: "123-456-7890",
-  };
+  name: string;
+  age: number;
+};
+
+/**
+ * The `SecondType` type is defined as an object with two properties:
+ * - `address`: A string representing the person's address.
+ * - `phone`: A string representing the person's phone number.
+ */
+type SecondType = {
+  address: string;
+  phone: string;
+};
+
+/**
+ * The `CombinedType` type is created using an intersection type,
+ * combining the properties of `FirstType` and `SecondType`.
+ * This means that `CombinedType` will have all properties from both types.
+ */
+type CombinedType = FirstType & SecondType;
+
+/**
+ * The `person` constant is created as an object adhering to the `CombinedType`.
+ * It includes properties from both `FirstType` and `SecondType`.
+ */
+const person: CombinedType = {
+  name: "John Doe", // Name from FirstType
+  age: 30, // Age from FirstType
+  address: "123 Main St", // Address from SecondType
+  phone: "123-456-7890", // Phone from SecondType
+};
