89b700f4883e06b3c3944ba1bf626726c531e844
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-21 23:14:19 +0500

    .

--- 50/q149/main.ts
+++ 50/q149/main.ts
@@ -1,7 +1,10 @@
+// Log a message indicating the start of the execution
 console.log("Start");
 
+// Schedule a callback function to be executed after a 1-second delay (1000 milliseconds)
 setTimeout(() => {
-  console.log("Callback executed");
+  console.log("Callback executed"); // This message will be logged after the delay
 }, 1000);
 
+// Log a message indicating the end of the execution
 console.log("End");
