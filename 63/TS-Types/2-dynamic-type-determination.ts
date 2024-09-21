0cde5cd32ae8f3c2c3a55794a74ad0a722ac238a
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-21 23:11:13 +0500

    .

--- 63/TS-Types/2-dynamic-type-determination.ts
+++ 63/TS-Types/2-dynamic-type-determination.ts
@@ -2,5 +2,13 @@
  * DYNAMIC TYPE DETERMINATION *
  ******************************/
 
-let greeting = "Hello, world!";
+/**
+ * In this example, TypeScript is automatically inferring the type of the `greeting` variable 
+ * based on the value assigned to it. Since `"Hello, world!"` is a string, TypeScript determines 
+ * the type of `greeting` as `string`. This is an example of dynamic type determination or 
+ * type inference, where the type is implicitly assigned by the TypeScript compiler.
+ */
+let greeting = "Hello, world!"; // TypeScript infers the type as `string`
+
+// Logging the value of the greeting to the console
 console.log(greeting);
