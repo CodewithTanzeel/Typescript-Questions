9623ac6c732ee0c62c06deb3faf05733fdf4a2c0
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-21 23:07:01 +0500

    .

--- 65/TS-Function/5-Anonymous-Function.ts
+++ 65/TS-Function/5-Anonymous-Function.ts
@@ -2,7 +2,18 @@
  * ANONYMOUS FUNCTION *
  **********************/
 
+/**
+ * This anonymous function is assigned to the constant `add`. It takes two numeric 
+ * parameters, `num1` and `num2`, and returns their sum. Anonymous functions are 
+ * functions without a name and are often used as expressions or callbacks.
+ * 
+ * @param {number} num1 - The first number to be added.
+ * @param {number} num2 - The second number to be added.
+ * @returns {number} - The sum of `num1` and `num2`.
+ */
 const add = function (num1: number, num2: number): number {
-    return num1 + num2;
-  };
-  console.log(add(10, 20)); // Output: 30
+  return num1 + num2;
+};
+
+// Calling the anonymous function `add` and logging the result
+console.log(add(10, 20));  // Output: 30
