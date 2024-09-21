750bfb4c3225ec6dd8fbd5f16084959ac8889451
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-21 23:03:13 +0500

    .

--- 65/TS-Function/1-Type-Annotations-With-Function.ts
+++ 65/TS-Function/1-Type-Annotations-With-Function.ts
@@ -2,16 +2,33 @@
  * TYPE ANNOTATIONS WITH FUNCTION *
  **********************************/
 
-// Function to calculate the area of a rectangle
+/**
+ * Function `calculateRectangleArea` calculates the area of a rectangle.
+ * It accepts two parameters:
+ * - `lengthRectAngle`: A number representing the length of the rectangle.
+ * - `widthOfRectAngle`: A number representing the width of the rectangle.
+ * 
+ * The function returns a `number` which is the calculated area.
+ */
 function calculateRectangleArea(
   lengthRectAngle: number,
   widthOfRectAngle: number
 ): number {
-  return lengthRectAngle * widthOfRectAngle;
+  return lengthRectAngle * widthOfRectAngle;  // Return the product of length and width
 }
 
-// Calling the function with valid arguments
-const lengthRectAngle = 5;
-const widthOfRectAngle = 3;
-const area = calculateRectangleArea(lengthRectAngle, widthOfRectAngle);
+/**
+ * Variables `lengthRectAngle` and `widthOfRectAngle` are declared to store
+ * the length and width values, respectively.
+ */
+const lengthRectAngle = 5;  // Length of the rectangle
+const widthOfRectAngle = 3;  // Width of the rectangle
+
+/**
+ * The `area` variable is assigned the result of calling the `calculateRectangleArea`
+ * function with valid arguments for length and width.
+ */
+const area = calculateRectangleArea(lengthRectAngle, widthOfRectAngle);  // Calculate the area
+
+// Output the calculated area of the rectangle
 console.log(`The area of the rectangle is: ${area}`);
