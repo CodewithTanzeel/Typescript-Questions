296418807b3ff397bb2bc1b487f14eba1bff7dee
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-21 23:10:12 +0500

    .

--- 63/TS-Types/1-type-annotations.ts
+++ 63/TS-Types/1-type-annotations.ts
@@ -2,19 +2,30 @@
  * TYPE ANNOTATIONS *
  ********************/
 
+/**
+ * Here, type annotations are used to explicitly define the data types of variables.
+ * `myNumberExOne` is annotated as a number and `myStringExOne` as a string. 
+ * This ensures that only values of the specified type can be assigned to these variables.
+ */
 let myNumberExOne: number = 42;
 let myStringExOne: string = "Hello, TypeScript";
 
+// Logging the values of the variables to the console
 console.log("My Number Is:", myNumberExOne);
 console.log("My String Is:", myStringExOne);
 
+/**
+ * Another way to define and assign the variables is by declaring them first with 
+ * type annotations and then assigning values separately. This provides flexibility 
+ * in cases where variables might need to be initialized later.
+ */
+let myNumberExTwo: number; // Variable declared with type annotation (number)
+let myStringExTwo: string; // Variable declared with type annotation (string)
 
-// Another way to write this code is as follows:
-let myNumberExTwo: number;
-let myStringExTwo: string;
-
+// Assigning values to the declared variables
 myNumberExTwo = 42;
 myStringExTwo = "Hello, TypeScript";
 
+// Logging the values of the variables to the console
 console.log("My Number Is:", myNumberExTwo);
 console.log("My String Is:", myStringExTwo);
