34b5f65ebb19ae6726b7c981446aac9310f7608a
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-21 23:05:51 +0500

    .

--- 65/TS-Function/4-Arrow-Function.ts
+++ 65/TS-Function/4-Arrow-Function.ts
@@ -2,5 +2,15 @@
  * ARROW FUNCTION *
  ******************/
 
+/**
+ * The `addWithArrow` is an arrow function that takes two numeric arguments, `num1` and `num2`, 
+ * and returns their sum. Arrow functions provide a concise syntax for function expressions.
+ * 
+ * @param {number} num1 - The first number to be added.
+ * @param {number} num2 - The second number to be added.
+ * @returns {number} - The sum of `num1` and `num2`.
+ */
 const addWithArrow = (num1: number, num2: number): number => num1 + num2;
-console.log(addWithArrow(10, 20)); // Output: 30
+
+// Calling the arrow function `addWithArrow` and logging the result
+console.log(addWithArrow(10, 20));  // Output: 30
