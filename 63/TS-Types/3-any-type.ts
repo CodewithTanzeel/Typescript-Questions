65e45f92159c171b153ee02823ea0aea5d4a4d4b
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-21 23:11:50 +0500

    .

--- 63/TS-Types/3-any-type.ts
+++ 63/TS-Types/3-any-type.ts
@@ -2,6 +2,15 @@
  * ANY TYPE *
  ************/
 
-let myVariable: any = 42;
-myVariable = "Hello, TypeScript";
-console.log(myVariable)
+/**
+ * In this example, the `any` type is used to indicate that a variable can hold a value of any type. 
+ * This disables TypeScript's type checking for that variable, allowing it to be reassigned 
+ * to different types without raising errors. The `myVariable` is first assigned a number 
+ * and later reassigned a string, demonstrating the flexibility of the `any` type.
+ */
+
+let myVariable: any = 42; // `myVariable` is initialized with a number
+myVariable = "Hello, TypeScript"; // Reassigned to a string without type error
+
+// Logging the current value of `myVariable` (which is now a string)
+console.log(myVariable); // Output: "Hello, TypeScript"
