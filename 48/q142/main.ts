7062d91401454f3ce69cf1d951232c43ee439a28
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-22 22:54:31 +0500

    .

--- 48/q142/main.ts
+++
@@ -1,8 +0,0 @@
-const promise = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve("Hello, World!");
-  }, 2000);
-});
-
-promise.then((message) => console.log(message));
-// After 2 seconds, "Hello, World!" will be logged to the console.
