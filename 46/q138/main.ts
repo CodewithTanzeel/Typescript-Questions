74d9788c9c39d25f7854eccd06c13cea5636109b
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-22 23:01:29 +0500

    .

--- 46/q138/main.ts
+++ 46/q138/main.ts
@@ -1,7 +1,10 @@
+// Log a message indicating the start of the script execution
 console.log("Before breakpoint");
 
+// Log a message where a breakpoint is set in the browser's developer tools
 console.log(
   "This line has a breakpoint set on it in the browser's developer tools"
 );
 
+// Log a message indicating that the script has continued execution after the breakpoint
 console.log("After breakpoint");
