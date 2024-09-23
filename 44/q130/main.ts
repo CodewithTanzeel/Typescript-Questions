6f5a3881ffe6f387470721ef85f835eadc34c112
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-23 22:27:08 +0500

    .

--- 44/q130/main.ts
+++ 44/q130/main.ts
@@ -1,3 +1,6 @@
+// Import the 'UpperCase' function from the './case' module
 import { UpperCase } from "./case";
 
+// Call the 'UpperCase' function with the argument 'heading' and log the result
 console.log(UpperCase("heading")); 
+// Expected output: 'HEADING' if the function converts the input string to uppercase
