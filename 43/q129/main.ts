9a90a158ca52fd5f1b60d650f488235c297e1089
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-23 22:38:15 +0500

    .

--- 43/q129/main.ts
+++ 43/q129/main.ts
@@ -1,21 +1,30 @@
 // Demonstrating 'this' in traditional vs. arrow functions
 const traditionalVsArrow = {
-  value: "traditionalVsArrow value",
+  value: "traditionalVsArrow value", // Property of the object
+
+  /**
+   * Traditional function that logs the value of 'this'.
+   * In traditional functions, 'this' refers to the object from which the function was called.
+   */
   traditionalFunction: function () {
     console.log("Traditional function:", this.value);
-    // 'this' refers to traditionalVsArrow object
   },
+
+  /**
+   * Arrow function that logs the value of 'this'.
+   * In arrow functions, 'this' is lexically bound, meaning it refers to the surrounding scope where the function was defined.
+   */
   arrowFunction: () => {
     console.log("Arrow function:", this.value);
-    // 'this' is not bound to traditionalVsArrow object
-    //  but to the scope in which traditionalVsArrow was defined
   },
 };
 
+// Calling the traditional function
 traditionalVsArrow.traditionalFunction();
 // Logs "traditionalVsArrow value"
 
+// Calling the arrow function
 traditionalVsArrow.arrowFunction();
 // Likely undefined, depending on the outer scope's 'this.value'
-// This code snippet illustrates the difference in how
-// 'this' is determined in traditional functions versus arrow functions.
+// This code snippet illustrates the difference in how 'this' is determined
+// in traditional functions versus arrow functions.
