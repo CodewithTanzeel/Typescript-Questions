54582c5947dae205fdafc37529c67d06ed8ae76b
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-23 22:23:53 +0500

    .

--- 45/q133/main.ts
+++ 45/q133/main.ts
@@ -1,9 +1,14 @@
+// Define an object 'user' with properties: 'name', 'email', and 'id'
 let user = {
     name: "ahmed",
     email: "ahmed@gmail.com",
     id: 10202
-};
-
-let convert = JSON.stringify(user); 
-console.log(convert)
-// {"name":"ahmed","email":"ahmed@gmail.com","id":10202}
+  };
+  
+  // Convert the 'user' object into a JSON string using JSON.stringify
+  let convert = JSON.stringify(user); 
+  
+  // Log the JSON string representation of the 'user' object
+  console.log(convert); 
+  // Output: {"name":"ahmed","email":"ahmed@gmail.com","id":10202}
+  
