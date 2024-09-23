1e0b5dd84cefc94207808678d090c4e149340f32
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-23 22:36:47 +0500

    .

--- 43/q128/main.ts
+++ 43/q128/main.ts
@@ -1,6 +1,11 @@
+/**
+ * Multiplies any number of parameters.
+ * @param {...number[]} numbers - A rest parameter representing the numbers to be multiplied.
+ * @returns {number} - The product of all the provided numbers.
+ */
 const multiplyParameters = (...numbers: number[]) =>
   numbers.reduce((total, number) => {
-    return total * number;
-  }, 1);
+    return total * number; // Multiplies each number with the running total.
+  }, 1); // Initializes total to 1 to avoid multiplication by 0.
 
-console.log(multiplyParameters(3, 3, 3)); // 27
+console.log(multiplyParameters(3, 3, 3)); // Output: 27 (3 * 3 * 3)
