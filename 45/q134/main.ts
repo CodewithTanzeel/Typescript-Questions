433c7e9b7a0d83bc5e6f6f8e1284defe76fb6714
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-23 22:24:38 +0500

    .

--- 45/q134/main.ts
+++ 45/q134/main.ts
@@ -1,5 +1,9 @@
+// Define a JSON string 'jsonString' representing an object with 'name', 'email', and 'id'
 const jsonString = '{"name":"ahmed","email":"ahmed@gmail.com","id":10202}';
 
+// Parse the JSON string into a JavaScript object using JSON.parse
 const convertJsObj = JSON.parse(jsonString);
 
+// Log the JavaScript object resulting from the parsed JSON string
 console.log(convertJsObj);
+// Output: { name: 'ahmed', email: 'ahmed@gmail.com', id: 10202 }
