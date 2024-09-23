3aea3329709ce88bd5ef920859f9eaaab45ea76f
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-23 22:27:40 +0500

    .

--- 44/q130/case.ts
+++ 44/q130/case.ts
@@ -1,3 +1,9 @@
+/**
+ * Converts a given string to uppercase.
+ * 
+ * @param {string} text - The input string to be converted.
+ * @returns {string} - The uppercase version of the input string.
+ */
 export function UpperCase(text: string): string {
-    return text.toUpperCase()
+    return text.toUpperCase();
 }
