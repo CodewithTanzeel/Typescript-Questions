ba6c1e77728f733c210df585846f8a56a7cd800d
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-23 22:31:41 +0500

    .

--- 44/q132/calculator.ts
+++ 44/q132/calculator.ts
@@ -1,3 +1,7 @@
+/**
+ * This class is exported as the default export from the module.
+ * It can be imported in other files without using curly braces.
+ */
 export default class Calculator {
-    // Class definition
-  }
+  // Class definition
+}
