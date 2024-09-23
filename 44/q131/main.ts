16c39ef6d57b39a8cee4bfc5c762cbe9a79a26aa
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-23 22:29:38 +0500

    .

--- 44/q131/main.ts
+++ 44/q131/main.ts
@@ -1,4 +1,14 @@
+/**
+ * Importing the Shop class from the 'class' module.
+ */
 import { Shop } from "./class";
 
+/**
+ * Creating a new instance of the Shop class with product name "Wood" and price 20.
+ */
 const slot = new Shop("Wood", 20);
-slot.displayNameAndPrice()
+
+/**
+ * Displaying the product name and calculated price using the displayNameAndPrice method.
+ */
+slot.displayNameAndPrice();
