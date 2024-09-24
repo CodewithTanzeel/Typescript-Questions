ae7835586816de8dad67f4c8e3f23bf635d1f522
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-24 22:28:20 +0500

    .

---
+++ 40/q119/main.ts
@@ -0,0 +1,12 @@
+let i = 1;
+// Initializing the variable 'i' with a value of 1 outside the loop
+
+while (i <= 5) { 
+  // Condition: loop continues as long as 'i' is less than or equal to 5
+
+  console.log(i, "Hello World"); 
+  // Logs the current value of 'i' followed by "Hello World"
+
+  i++; 
+  // Increment: Increases the value of 'i' by 1 after each iteration
+}
