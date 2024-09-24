49e5301d51e4cc95d68ae5e467bd8ef445ec867e
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-24 22:28:07 +0500

    .

--- 40/q118/main.ts
+++ 40/q118/main.ts
@@ -1,6 +1,8 @@
-// Using For Loop
+// Using For Loop to print numbers from 1 to 10
+for (let i = 1;     // initializer: sets i to 1
+    i <= 10;       // condition: loop continues while i is less than or equal to 10
+    i++) {         // increment: increases i by 1 after each iteration
 
-for(let i = 1;     i <= 10;      i++) {
-// initializer    condition   increment/decrement
-    console.log(i);
+   // Log the current value of i in each iteration
+   console.log(i);
 }
--- 40/q119/main.ts
+++
@@ -1,7 +0,0 @@
-let i = 1;
-// initializing a variable outside
-
-while (i <= 5) {
-  console.log(i, "Hello World");
-  i++;
-}
