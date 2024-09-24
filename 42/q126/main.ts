a02e89c7d0f3ea5996a9c173d99920b9a099414b
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-24 22:23:05 +0500

    .

--- 42/q126/main.ts
+++ 42/q126/main.ts
@@ -1,14 +1,22 @@
+// Define an object 'myObject' with a 'property' and two methods.
 const myObject = {
-  property: "john",
-  outerMethod: function () {
-    console.log(this.property);
+  property: "john", // A string property assigned to 'john'.
 
+  /**
+   * Method 'outerMethod' that logs the value of 'this.property'
+   * and calls the 'innerMethod'.
+   */
+  outerMethod: function (): void {
+    console.log(this.property); // Logs the 'property' value from 'myObject' ('john').
+
+    // Define an inner arrow function 'innerMethod'.
     const innerMethod = () => {
-      console.log(this.property);
+      console.log(this.property); // Logs 'this.property' which still refers to 'myObject' due to arrow function binding.
     };
 
-    innerMethod();
+    innerMethod(); // Calls 'innerMethod'.
   },
 };
 
+// Invoke 'outerMethod', which will log 'john' twice.
 myObject.outerMethod();
