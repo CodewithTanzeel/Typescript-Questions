d91517d533bd4a111702c24378a6ecd32efa155c
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-24 22:29:19 +0500

    .

--- 40/q120/main.ts
+++ 40/q120/main.ts
@@ -4,7 +4,11 @@ let favoriteMovies: string[] = [
   "The Smurfs",
   "Minions",
 ];
+// Array of favorite movies, each movie title is a string
 
 for (let movies of favoriteMovies) {
+  // Looping through each element in the 'favoriteMovies' array
+
   console.log(movies);
+  // Logs the current movie title in each iteration
 }
