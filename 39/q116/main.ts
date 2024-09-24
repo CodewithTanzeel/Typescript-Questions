e8b310e955ffdfdf55158197573e50b0cf63b33d
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-24 22:31:43 +0500

    .

--- 39/q116/main.ts
+++ 39/q116/main.ts
@@ -1,28 +1,36 @@
 function season(month: number) {
+  // Function to determine the season based on the given month number
+
   switch (month) {
-    case 12:
-    case 1:
-    case 2:
-      console.log("Winter");
-      break;
-    case 3:
-    case 4:
-    case 5:
-      console.log("Spring");
-      break;
-    case 6:
-    case 7:
-    case 8:
-      console.log("Summer");
-      break;
-    case 9:
-    case 10:
-    case 11:
-      console.log("Autumn");
-      break;
-    default:
-      console.log("Invalid month number");
+      // Evaluating the input number 'month'
+      case 12: // December
+      case 1:  // January
+      case 2:  // February
+          console.log("Winter"); // If month is December, January, or February, log "Winter"
+          break; // Exit the switch block
+      
+      case 3:  // March
+      case 4:  // April
+      case 5:  // May
+          console.log("Spring"); // If month is March, April, or May, log "Spring"
+          break; // Exit the switch block
+      
+      case 6:  // June
+      case 7:  // July
+      case 8:  // August
+          console.log("Summer"); // If month is June, July, or August, log "Summer"
+          break; // Exit the switch block
+      
+      case 9:  // September
+      case 10: // October
+      case 11: // November
+          console.log("Autumn"); // If month is September, October, or November, log "Autumn"
+          break; // Exit the switch block
+      
+      default:
+          console.log("Invalid month number"); // Log error message for invalid input
   }
 }
 
-season(6); // Summer
+// Example call to the function
+season(6); // Outputs: Summer
