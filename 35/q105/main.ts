b4b51b4fe6700f8e150f2e7df04a088cc46ce133
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-25 22:58:21 +0500

    .

--- 35/q105/main.ts
+++ 35/q105/main.ts
@@ -1,7 +1,12 @@
+/**
+ * Simulates rolling a six-sided dice.
+ * @returns A random integer between 1 and 6, inclusive.
+ */
 function rollDice() {
   // Generate a random number between 1 and 6
   const randomNumber = Math.floor(Math.random() * 6) + 1;
   return randomNumber;
 }
 
+// Test the function by logging the result of rolling the dice
 console.log(rollDice());
